4c75741e65156aadd27afba6a5a855f45efe6264
Noryuxd <rabiiababsa10@gmail.com>
2023-10-31 19:15:07 +0100

    real

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,37 +1,21 @@
-import React, { useState } from "react";
-
-const Hero = () => {
-  const CLIENT_ID = "afd5b7c9c4134ac3a96b54e2bd5479e8";
-  const REDIRECT_URI = "http://localhost:5173/";
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-  const RESPONSE_TYPE = "token";
-
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLogin = () => {
-    window.location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem("acces_token");
-    setIsLoggedIn(false);
-  };
-
+const Hero = ({searchResults}) => {
   return (
-    <div className="p-5 mt-56 text-center">
-      <h1 className="text-5xl pb-10">ABSMusic</h1>
-      <p className="text-4xl pb-5">
-        Discover your favorite music in 30 seconds only!
-      </p>
-      {isLoggedIn ? (
-        <button onClick={handleLogout} className="text-3xl text-red-600">
-          Logout
-        </button>
-      ) : (
-        <a href="#" onClick={handleLogin} className="text-3xl text-green-600">
-          Login to Spotify
-        </a>
-      )}
+    <div>
+      <ul>
+        {searchResults?.map((track) => (
+          <>
+            <li key={track?.id}>{track?.name}</li>
+            <iframe
+              src={`https://open.spotify.com/embed/track/${track.id}`}
+              width="300"
+              height="200"
+              frameBorder={0}
+              allowTransparency={true}
+              allow="encrypted-media"
+            ></iframe>
+          </>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,9 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import InputBase from "@mui/material/InputBase";
 import SearchIcon from "@mui/icons-material/Search";
+import { useState } from "react";
+import axios from "axios";
+import Hero from "./Hero";
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -49,31 +52,62 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }));
 
 export default function Navbar() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+
+    // Add your Spotify API access token here
+    const accessToken = "afd5b7c9c4134ac3a96b54e2bd5479e8";
+
+    try {
+      const response = await axios.get(
+        `https://api.spotify.com/v1/search?q=${searchQuery}&type=track`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      // Process the response and update the searchResults state with the data
+      setSearchResults(response.data.tracks.items);
+    } catch (error) {
+      console.error("Error fetching data from Spotify API:", error);
+    }
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-          >
-            ABSMusic
-          </Typography>
-          <form>
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Search..."
-                inputProps={{ "aria-label": "search" }}
-              />
-            </Search>
-          </form>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            >
+              ABSMusic
+            </Typography>
+            <form onSubmit={handleSearch}>
+              <Search>
+                <SearchIconWrapper>
+                  <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search..."
+                  inputProps={{ "aria-label": "search" }}
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
+              </Search>
+            </form>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Hero searchResults={searchResults} />
+    </>
   );
 }
